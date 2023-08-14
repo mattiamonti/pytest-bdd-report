b56b9b7c1ad4dcba89d9bf18d9be8ed44c728201
Mattia Monti <77688571+mattiamonti@users.noreply.github.com>
2023-08-14 16:07:53 +0200

    created collector test cases

---
+++ tests/tests_collector/test_scenario_aggregator.py
@@ -0,0 +1,77 @@
+import pytest
+from collector.scenario_aggregator import ScenarioAggregator
+
+
+@pytest.fixture
+def mock_steps_information():
+    return [
+        {
+            'feature': 'Feature 1',
+            'scenario': 'Scenario A',
+            'type': 'Given',
+            'step': 'Some given step',
+            'status': 'passed',
+            'nodeid': 'node1',
+            'exception': ''
+        },
+        {
+            'feature': 'Feature 1',
+            'scenario': 'Scenario A',
+            'type': 'When',
+            'step': 'Some when step',
+            'status': 'passed',
+            'nodeid': 'node2',
+            'exception': ''
+        },
+        {
+            'feature': 'Feature 1',
+            'scenario': 'Scenario A',
+            'type': 'Then',
+            'step': 'Some then step',
+            'status': 'passed',
+            'nodeid': 'node3',
+            'exception': ''
+        }, {
+            'feature': 'Feature 1',
+            'scenario': 'Scenario B',
+            'type': 'Given',
+            'step': 'Some given step',
+            'status': 'passed',
+            'nodeid': 'node1',
+            'exception': ''
+        },
+        {
+            'feature': 'Feature 1',
+            'scenario': 'Scenario B',
+            'type': 'When',
+            'step': 'Some when step',
+            'status': 'passed',
+            'nodeid': 'node2',
+            'exception': ''
+        }
+    ]
+
+
+def test_aggregate_steps_into_scenario(mock_steps_information):
+    aggregator = ScenarioAggregator()
+
+    aggregated_results = aggregator.aggregate_steps_into_scenario(
+        mock_steps_information)
+
+    # Test if the scenario is aggregated correctly
+    assert 'Feature 1 - Scenario A' in aggregated_results
+    assert len(aggregated_results['Feature 1 - Scenario A']['steps']) == 3
+    assert len(aggregated_results['Feature 1 - Scenario B']['steps']) == 2
+
+
+def test_aggregated_steps_content(mock_steps_information):
+    aggregator = ScenarioAggregator()
+
+    aggregated_results = aggregator.aggregate_steps_into_scenario(
+        mock_steps_information)
+
+    scenario_a_steps = aggregated_results['Feature 1 - Scenario A']['steps']
+
+    assert scenario_a_steps[0]['type'] == 'Given' and scenario_a_steps[0]['nodeid'] == 'node1'
+    assert scenario_a_steps[1]['type'] == 'When' and scenario_a_steps[1]['nodeid'] == 'node2'
+    assert scenario_a_steps[2]['type'] == 'Then' and scenario_a_steps[2]['nodeid'] == 'node3'
---
+++ tests/tests_collector/test_scenario_result_merger.py
@@ -0,0 +1,72 @@
+import pytest
+from collector.scenario_result_merger import ScenarioAndResultMerger, LengthMismatchError
+
+
+@pytest.fixture
+def mock_aggregated_scenarios():
+    return {
+        'Feature 1 - Scenario A': {
+            'feature': 'Feature 1',
+            'scenario': 'Scenario A',
+            'steps': ['Step 1', 'Step 2', 'Step 3']
+        }
+    }
+
+
+@pytest.fixture
+def mock_tests_results():
+    return [
+        {
+            'nodeid': 'node1',
+            'duration': 0.5,
+            'outcome': 'passed',
+            'longrepr': 'Test passed successfully.'
+        },
+    ]
+
+
+def test_merge_scenario_and_result_length(mock_aggregated_scenarios, mock_tests_results):
+    merger = ScenarioAndResultMerger()
+    merged_results = merger.merge_scenario_and_result(
+        mock_aggregated_scenarios, mock_tests_results)
+
+    assert len(merged_results) == len(mock_aggregated_scenarios.values())
+    assert len(merged_results) == len(mock_tests_results)
+    assert merged_results[0]['feature'] == 'Feature 1'
+    assert merged_results[0]['scenario'] == 'Scenario A'
+
+
+def test_merge_scenario_and_result_content(mock_aggregated_scenarios, mock_tests_results):
+    merger = ScenarioAndResultMerger()
+    merged_results = merger.merge_scenario_and_result(
+        mock_aggregated_scenarios, mock_tests_results)
+
+    assert merged_results[0]['feature'] == 'Feature 1'
+    assert merged_results[0]['scenario'] == 'Scenario A'
+    assert merged_results[0]['nodeid'] == 'node1'
+    assert merged_results[0]['outcome'] == 'passed'
+    assert merged_results[0]['duration'] == 0.5
+
+
+def test_merge_scenario_and_result_mismatch(mock_aggregated_scenarios):
+    # tests_results too longo to merge
+    mock_tests_results = [
+        {
+            'nodeid': 'node1',
+            'duration': 0.5,
+            'outcome': 'passed',
+            'longrepr': 'Test passed successfully.'
+        },
+        {
+            'nodeid': 'node2',
+            'duration': 0.5,
+            'outcome': 'failed',
+            'longrepr': 'Test failed'
+        }
+    ]
+
+    merger = ScenarioAndResultMerger()
+
+    with pytest.raises(LengthMismatchError):
+        merger.merge_scenario_and_result(
+            mock_aggregated_scenarios, mock_tests_results)
