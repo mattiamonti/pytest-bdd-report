95249cea22b20c86164fa70a0f691475711db21c
Mattia Monti <77688571+mattiamonti@users.noreply.github.com>
2023-09-01 15:41:29 +0200

    improved tests structure and added more test

--- tests/test_plugin/test_plugin_config.py
+++ tests/test_plugin/test_plugin_config.py
@@ -3,37 +3,48 @@ import pytest
 import textwrap
 
 
-
 def test_arguments_in_help(testdir):
-    res = testdir.runpytest('--help')
-    res.stdout.fnmatch_lines([
-        '*bdd-json*',
-        '*report*',
-    ])
+    res = testdir.runpytest("--help")
+    res.stdout.fnmatch_lines(
+        [
+            "*bdd-json*",
+            "*report*",
+        ]
+    )
+
 
 def test_json_message_in_stdout(testdir):
-    res = testdir.runpytest('--bdd-json')
-    res.stdout.fnmatch_lines([
-        '*JSON with the tests result created successfully*',
-    ])
+    res = testdir.runpytest("--bdd-json")
+    res.stdout.fnmatch_lines(
+        [
+            "*JSON with the tests result created successfully*",
+        ]
+    )
+
 
 def test_report_message_in_stdout(testdir):
     arg = "test"
-    res = testdir.runpytest(f'--report={arg}')
-    res.stdout.fnmatch_lines([
-        f'*Report created at: {arg}*.html*',
-    ])
+    res = testdir.runpytest(f"--report={arg}")
+    res.stdout.fnmatch_lines(
+        [
+            f"*Report created at: {arg}*.html*",
+        ]
+    )
+
 
 def test_report_message_in_stdout_with_html(testdir):
     arg = "test.html"
-    res = testdir.runpytest(f'--report={arg}')
-    res.stdout.fnmatch_lines([
-        f'*Report created at: {arg}*',
-    ])
+    res = testdir.runpytest(f"--report={arg}")
+    res.stdout.fnmatch_lines(
+        [
+            f"*Report created at: {arg}*",
+        ]
+    )
+
 
 # fixture for an example test that pass
 @pytest.fixture()
-def sample_test_pass(testdir):
+def sample_test(testdir):
     testdir.makefile(
         ".feature",
         scenario=textwrap.dedent(
@@ -41,7 +52,9 @@ def sample_test_pass(testdir):
             Feature: Testing
                 Scenario: Test scenario
                     Given I have a scenario
-                    Then Should be created
+                    When I start the test
+                    And I know it will fails
+                    Then It fails
             """
         ),
     )
@@ -50,7 +63,7 @@ def sample_test_pass(testdir):
         textwrap.dedent(
             """\
         import pytest
-        from pytest_bdd import given, then, scenario
+        from pytest_bdd import given, when, then, scenario
 
         @scenario("scenario.feature", "Test scenario")
         def test_scenario():
@@ -59,47 +72,16 @@ def sample_test_pass(testdir):
         @given("I have a scenario")
         def _():
             pass
-
-        @then('Should be created')
+            
+        @when("I start the test")
         def _():
-            assert True
-
-        """
-        )
-    )
-
-    return testdir
-
-# fixture for an example test that fail
-@pytest.fixture
-def sample_test_fail(testdir):
-    testdir.makefile(
-        ".feature",
-        scenario=textwrap.dedent(
-            """\
-            Feature: Testing
-                Scenario: Test scenario
-                    Given I have a scenario
-                    Then Should be created
-            """
-        ),
-    )
-
-    testdir.makepyfile(
-        textwrap.dedent(
-            """\
-        import pytest
-        from pytest_bdd import given, then, scenario
-
-        @scenario("scenario.feature", "Test scenario")
-        def test_scenario():
             pass
-
-        @given("I have a scenario")
+        
+        @when("I know it will fails")
         def _():
             pass
 
-        @then('Should be created')
+        @then('It fails')
         def _():
             assert False
 
@@ -110,73 +92,67 @@ def sample_test_fail(testdir):
     return testdir
 
 
-def test_create_json(sample_test_pass):
-    sample_test_pass.runpytest('--bdd-json')
-    assert (sample_test_pass.tmpdir / 'session_finish_results.json').exists()
+def test_create_json(sample_test):
+    sample_test.runpytest("--bdd-json")
+    assert (sample_test.tmpdir / "session_finish_results.json").exists()
 
 
-def test_json_content_scenario_pass(sample_test_pass):
-    sample_test_pass.runpytest('--bdd-json')
+def test_json_steps_keyword(sample_test):
+    sample_test.runpytest("--bdd-json")
 
-    with open(str(sample_test_pass.tmpdir / "session_finish_results.json")) as f:
+    with open(str(sample_test.tmpdir / "session_finish_results.json")) as f:
         data = json.load(f)
-    
-    assert data[0]['nodeid'] != ""
-    assert data[0]['feature'] == "Testing"
-    assert data[0]['scenario'] == "Test scenario"
-    assert data[0]['outcome'] == "passed"
-    assert data[0]['duration'] != 0
-    assert len(data[0]['steps']) == 2
 
+    steps = data[0]['steps']
+    assert steps[0]['type'] == "Given"
+    assert steps[1]['type'] == "When"
+    assert steps[2]['type'] == "And"
+    assert steps[3]['type'] == "Then"
 
-def test_json_content_steps_pass(sample_test_pass):
-    sample_test_pass.runpytest('--bdd-json')
+def test_json_steps_argument(sample_test):
+    sample_test.runpytest("--bdd-json")
 
-    with open(str(sample_test_pass.tmpdir / "session_finish_results.json")) as f:
+    with open(str(sample_test.tmpdir / "session_finish_results.json")) as f:
         data = json.load(f)
-    
-    assert len(data[0]['steps']) == 2
-    assert data[0]['steps'][0]['nodeid'] != ""
-    assert data[0]['steps'][0]['type'] == "Given"
-    assert data[0]['steps'][0]['step'] == "I have a scenario"
-    assert data[0]['steps'][0]['status'] == "passed"
-    assert data[0]['steps'][1]['nodeid'] != ""
-    assert data[0]['steps'][1]['type'] == "Then"
-    assert data[0]['steps'][1]['step'] == "Should be created"
-    assert data[0]['steps'][1]['status'] == "passed"
 
+    steps = data[0]['steps']
+    assert steps[0]['step'] == "I have a scenario"
+    assert steps[1]['step'] == "I start the test"
+    assert steps[2]['step'] == "I know it will fails"
+    assert steps[3]['step'] == "It fails"
 
-def test_json_content_scenario_fail(sample_test_fail):
-    res = sample_test_fail.runpytest('--bdd-json')
+def test_json_steps_status(sample_test):
+    sample_test.runpytest("--bdd-json")
 
-    with open(str(sample_test_fail.tmpdir / "session_finish_results.json")) as f:
+    with open(str(sample_test.tmpdir / "session_finish_results.json")) as f:
         data = json.load(f)
-    
-    res.assert_outcomes(failed=1)
-    
-    assert data[0]['nodeid'] != ""
-    assert data[0]['feature'] == "Testing"
-    assert data[0]['scenario'] == "Test scenario"
-    assert data[0]['outcome'] == "failed"
-    assert data[0]['duration'] != 0
 
+    steps = data[0]['steps']
+    assert steps[0]['status'] == "passed"
+    assert steps[1]['status'] == "passed"
+    assert steps[2]['status'] == "passed"
+    assert steps[3]['status'] == "failed"
 
-def test_json_content_steps_fail(sample_test_fail):
-    res = sample_test_fail.runpytest('--bdd-json')
+def test_json_steps_nodeid(sample_test):
+    sample_test.runpytest("--bdd-json")
 
-    with open(str(sample_test_fail.tmpdir / "session_finish_results.json")) as f:
+    with open(str(sample_test.tmpdir / "session_finish_results.json")) as f:
         data = json.load(f)
-    
-    res.assert_outcomes(failed=1)
-    
-    assert len(data[0]['steps']) == 2
-    assert data[0]['steps'][0]['nodeid'] != ""
-    assert data[0]['steps'][0]['type'] == "Given"
-    assert data[0]['steps'][0]['step'] == "I have a scenario"
-    assert data[0]['steps'][0]['status'] == "passed"
-    assert data[0]['steps'][1]['nodeid'] != ""
-    assert data[0]['steps'][1]['type'] == "Then"
-    assert data[0]['steps'][1]['step'] == "Should be created"
-    assert data[0]['steps'][1]['status'] == "failed"
 
+    steps = data[0]['steps']
+    assert steps[0]['nodeid'] != "" 
+    assert steps[1]['nodeid'] != "" 
+    assert steps[2]['nodeid'] != "" 
+    assert steps[3]['nodeid'] != ""
 
+def test_json_steps_exception(sample_test):
+    sample_test.runpytest("--bdd-json")
+
+    with open(str(sample_test.tmpdir / "session_finish_results.json")) as f:
+        data = json.load(f)
+
+    steps = data[0]['steps']
+    assert steps[0]['exception'] == ""
+    assert steps[1]['exception'] == ""
+    assert steps[2]['exception'] == ""
+    assert steps[3]['exception'] == "assert False"
