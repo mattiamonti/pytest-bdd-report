ea2e9f5ad08efb348058ef8d8d4c943bc2027188
Mattia Monti <77688571+mattiamonti@users.noreply.github.com>
2023-09-20 17:22:01 +0200

    used interface

--- src/pytest_bdd_report/summary/summary_generator.py
+++ src/pytest_bdd_report/summary/summary_generator.py
@@ -1,3 +1,4 @@
+from pytest_bdd_report.interfaces import IReport
 from pytest_bdd_report.summary.summary import Summary
 from pytest_bdd_report.report import Report
 
@@ -6,16 +7,16 @@ class SummaryGenerator:
     def __init__(self):
         self.summary = Summary()
 
-    def populate_summary(self, report: Report) -> Summary:
+    def populate_summary(self, report: IReport) -> Summary:
         self._set_report_title(report)
         self._get_test_statistics(report)
         self._get_total_duration(report)
         return self.summary
 
-    def _set_report_title(self, report: Report) -> None:
+    def _set_report_title(self, report: IReport) -> None:
         self.summary.report_title = report.title
 
-    def _get_test_statistics(self, report: Report) -> None:
+    def _get_test_statistics(self, report: IReport) -> None:
         for feature in report.features:
             self.summary.total_test += feature.total_tests
             self.summary.test_passed += feature.passed_tests
@@ -25,7 +26,7 @@ class SummaryGenerator:
     def _get_start_time(self) -> None:
         ...
 
-    def _get_total_duration(self, report: Report) -> None:
+    def _get_total_duration(self, report: IReport) -> None:
         duration = 0.0
         for feature in report.features:
             duration += feature.duration
