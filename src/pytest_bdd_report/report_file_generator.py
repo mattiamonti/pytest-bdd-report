5989722e9c6b095b754ad77a55baf5a547f1d9e2
Mattia Monti <77688571+mattiamonti@users.noreply.github.com>
2023-09-22 17:43:12 +0200

    little improvements

--- src/pytest_bdd_report/report_file_generator.py
+++ src/pytest_bdd_report/report_file_generator.py
@@ -7,24 +7,34 @@ from pytest_bdd_report.renderer import FeatureRenderer
 class ReportFileGenerator:
     def __init__(self) -> None:
         self.report_content = ""
-        pass
 
     def create_report_file(self, report: IReport, path: str) -> None:
         """
         Create the report in the provided file path.
         """
-        rendered_features = FeatureRenderer().render(report.features, FeatureTemplate())
+        rendered_features = self._render_features(report)
         self.report_content = self._render_summary(rendered_features)
         self._save_report_to_file(path)
 
     @staticmethod
+    def _render_features(report: IReport) -> str:
+        """
+        Render the features and return them as a string.
+        """
+        feature_renderer = FeatureRenderer()
+        return feature_renderer.render(report.features, FeatureTemplate())
+
+    @staticmethod
     def _render_summary(data: str) -> str:
         """
-        Render the summary and append it on the report content
+        Render the summary and return it as a string.
         """
         summary_template = SummaryTemplate()
         return summary_template.render_template(data)
 
     def _save_report_to_file(self, path: str) -> None:
+        """
+        Save the report content to a file.
+        """
         with open(path, "w") as f:
             f.write(self.report_content)
