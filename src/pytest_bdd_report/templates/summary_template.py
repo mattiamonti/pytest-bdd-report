5a78ccb948f759d3f9d6f4b09e36c9643b5509d5
mattiamonti <mattiamonti2001@gmail.com>
2025-07-09 20:04:24 +0200

    Redesigned and implemented new top area, added fail_rate information

--- src/pytest_bdd_report/templates/summary_template.py
+++ src/pytest_bdd_report/templates/summary_template.py
@@ -8,12 +8,14 @@ class SummaryTemplate(BaseTemplate):
         super().__init__(self.path)
 
     def render_template(self, data: Summary, **kwargs) -> str:
+        fail_rate = round(data.tests_failed / data.total_tests * 100, 0)
         return self.template.render(
             total=data.total_tests,
             passed=data.tests_passed,
             failed=data.tests_failed,
             skipped=data.tests_skipped,
-            percentage=data.percentage_tests_passed,
+            pass_rate=data.percentage_tests_passed,
+            fail_rate=fail_rate,
             duration=round(data.total_duration, 5),
             top_feature_fail=data.top_feature_fail,
         )
