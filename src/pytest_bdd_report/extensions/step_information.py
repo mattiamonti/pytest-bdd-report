326542b85eefbd7a63a0d3c78da5da4562a1af56
mattiamonti <mattiamonti2001@gmail.com>
2026-04-01 19:44:53 +0200

    Create step_information.py

---
+++ src/pytest_bdd_report/extensions/step_information.py
@@ -0,0 +1,52 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class StepInformation:
+    step_keyword: str
+    step_name: str
+    text: str | None
+    json: dict | None
+
+
+class StepInformationRepo:
+    def __init__(self) -> None:
+        self.repo: list[StepInformation] = []
+
+    def add(self, step_keyword: str, step_name: str, information: str | dict) -> None:
+        """
+        Adds a new step information to the repository.
+
+        Raises:
+            ValueError: If the steps has already information attached.
+        """
+        if self.exists(step_keyword, step_name):
+            raise ValueError(
+                f"The step '{step_keyword} {step_name}' has already information attached."
+            )
+        step_information = StepInformation(step_keyword, step_name, None, None)
+        # TODO refactor della logica usando un saver strategy?
+        if isinstance(information, str):
+            step_information.text = information
+        if isinstance(information, dict):
+            step_information.json = information
+
+        self.repo.append(step_information)
+
+    def get(self, step_keyword: str, step_name: str) -> StepInformation | None:
+        """
+        Returns the saved step information if attached.
+        """
+        for item in self.repo:
+            if item.step_keyword == step_keyword and item.step_name == step_name:
+                return item
+        return None
+
+    def exists(self, step_keyword: str, step_name: str) -> bool:
+        return any(
+            item.step_keyword == step_keyword and item.step_name == step_name
+            for item in self.repo
+        )
+
+
+step_information_repo = StepInformationRepo()
