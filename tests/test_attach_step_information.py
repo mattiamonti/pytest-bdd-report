e0bf5200d25b9b321e81308cc43b717b30a9d109
mattiamonti <mattiamonti2001@gmail.com>
2026-04-03 22:24:13 +0200

    Update test_attach_step_information.py

--- tests/test_attach_step_information.py
+++ tests/test_attach_step_information.py
@@ -146,3 +146,41 @@ def test_attach_text_and_json_to_step():
     assert step_info.text != []
     assert formatted_json_info in step_info.json
     assert text_info in step_info.text
+
+
+@given(
+    text_informations=st.lists(elements=st.text(min_size=1), min_size=1),
+    json_informations=st.lists(
+        elements=st.dictionaries(
+            keys=st.text(min_size=1),
+            values=st.text(min_size=1),
+            min_size=1,
+        ),
+        min_size=1,
+    ),
+)
+def test_attach_multiple_text_and_json_to_step(
+    text_informations: list[str], json_informations: list[dict]
+):
+    step_keyword = "Given"
+    step_name = "Step name"
+
+    for text_info in text_informations:
+        attach.text_to_step(text_info, step_keyword, step_name)
+
+    for json_info in json_informations:
+        attach.json_to_step(json_info, step_keyword, step_name)
+
+    assert len(step_information_repo.repo) == 1
+    step_info = step_information_repo.get(step_keyword, step_name)
+    assert step_info is not None
+    step_information_repo.repo.remove(step_info)  # Clean the repository after the test
+    assert step_info.step_keyword == step_keyword
+    assert step_info.step_name == step_name
+    assert len(step_info.text) == len(text_informations)
+    for text_info in text_informations:
+        assert text_info in step_info.text
+    assert len(step_info.json) == len(json_informations)
+    for json_info in json_informations:
+        formatted_json_info = json.dumps(json_info, indent=2).strip()
+        assert formatted_json_info in step_info.json
