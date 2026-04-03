aeef3de9fd7fa50d4e62472b26bd97fb0c777ccd
mattiamonti <mattiamonti2001@gmail.com>
2026-04-03 22:47:27 +0200

    Refactored step information logic with strategy pattern

--- src/pytest_bdd_report/extensions/step_information.py
+++ src/pytest_bdd_report/extensions/step_information.py
@@ -1,5 +1,6 @@
 import json
 from dataclasses import dataclass
+from typing import Any, Protocol
 
 
 @dataclass
@@ -10,9 +11,26 @@ class StepInformation:
     json: list[str]
 
 
+class StepInformationSaverStrategy(Protocol):
+    def save(self, step_information: StepInformation, data: Any) -> StepInformation: ...
+
+
+class TextStepInformationSaver:
+    def save(self, step_information: StepInformation, data: str) -> StepInformation:
+        step_information.text.append(data)
+        return step_information
+
+
+class JsonStepInformationSaver:
+    def save(self, step_information: StepInformation, data: dict) -> StepInformation:
+        step_information.json.append(json.dumps(data, indent=2).strip())
+        return step_information
+
+
 class StepInformationRepo:
     def __init__(self) -> None:
         self.repo: list[StepInformation] = []
+        self._savers: dict[str, StepInformationSaverStrategy] = {}
 
     def add(self, step_keyword: str, step_name: str, information: str | dict) -> None:
         """
@@ -24,16 +42,11 @@ class StepInformationRepo:
             if not step_information:
                 return
             self.repo.remove(step_information)
-        if isinstance(information, str):
-            if not information:
-                return
-            step_information.text.append(information)
-        if isinstance(information, dict):
-            if not information:
-                return
-            step_information.json.append(json.dumps(information, indent=2).strip())
 
-        self.repo.append(step_information)
+        if not information:
+            return
+        saver_strategy = self.get_saver(information.__class__.__name__)
+        self.repo.append(saver_strategy.save(step_information, information))
 
     def get(self, step_keyword: str, step_name: str) -> StepInformation | None:
         """
@@ -50,5 +63,20 @@ class StepInformationRepo:
             for item in self.repo
         )
 
+    def register_saver(
+        self, saver: StepInformationSaverStrategy, for_type: str
+    ) -> None:
+        self._savers[for_type] = saver
+
+    def get_saver(self, for_type: str) -> StepInformationSaverStrategy:
+        saver = self._savers.get(for_type)
+        if not saver:
+            raise RuntimeWarning(
+                f"No information saver strategy register for the image type {for_type}. Try to register a saver with the .register_saver method of this class."
+            )
+        return saver
+
 
 step_information_repo = StepInformationRepo()
+step_information_repo.register_saver(TextStepInformationSaver(), "str")
+step_information_repo.register_saver(JsonStepInformationSaver(), "dict")
